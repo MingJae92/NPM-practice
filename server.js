dbfc3a011d24d9e0c2b7eb1cd04b92b7b5b84b52
Ming Chi <wong_mingchi@yahoo.co.uk>
2021-05-18 15:11:19 +0100

    server.js updated

--- server.js
+++ server.js
@@ -56,8 +56,7 @@ if (!process.env.DISABLE_XORIGIN) {
 // })
 
 var port = process.env.PORT || 3000;
-
-bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
-  bGround.log('Node.js listening on port' + port + '...');
+bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
+  bGround.log('Node is listening on port '+ port + '...')
 });
 
