a012b4c1d4d2e0f9dadd3b7bc259e71fa9bc3f97
Ming Chi <wong_mingchi@yahoo.co.uk>
2021-05-19 03:39:57 +0100

    middle wear chain added

--- myApp.js
+++ myApp.js
@@ -35,6 +35,17 @@ app.get("/json", function(req, res){
         res.json(jsonResponse);    
 });
 
+function getTheCurrentTimeString(){
+    return new Date().toString();
+}
+
+app.get("/now", function(req, res, next){
+    req.time = getTheCurrentTimeString();
+    next();
+}, function(req, res){
+    res.json({time:req.time});
+});
+
 
 
 
