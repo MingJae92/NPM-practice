38f01eeaff36ebecc4fb530c65310176fcb1f9a8
Ming Chi <wong_mingchi@yahoo.co.uk>
2021-05-19 01:32:59 +0100

    json path added to myApp.js

--- myApp.js
+++ myApp.js
@@ -12,6 +12,14 @@ app.get("/", function(req, res){
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", function(req, res){
+    res.json(
+        {
+            "message": "Hello json"
+        }
+    );
+});
+
 
 
 
