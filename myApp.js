9da7a0eb7766438043264a1aa836ba2fa7625b76
Ming Chi <wong_mingchi@yahoo.co.uk>
2021-05-21 00:44:33 +0100

    name path added to post method

--- myApp.js
+++ myApp.js
@@ -58,10 +58,9 @@ app.get("/:word/echo", function(req, res){
 
 app.get("/name", function(req, res){
     res.json({name:req.query.first + " " + req.query.last});
-
 });
 
-app.post(function(req, res){
+app.post("/name", function(req, res){
     res.json({name: req.body.first + " " +req.body.last});
 });
 
